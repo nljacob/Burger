beeef6b73a4af9e250db4acc4006582390cc03bf
nljacob <neehaljacob@gmail.com>
2018-04-21 20:56:04 -0700

    view burger and add burger working

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Yummy Burger');
+
+
